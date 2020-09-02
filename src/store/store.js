1861e9e662d4f8889347412577e3e0a2f31789f6
Chris Bell <cb.camellia@gmail.com>
2020-09-02 15:53:21 -0700

    Add rootReducer, initialState and store

--- src/store/store.js
+++ src/store/store.js
@@ -4,3 +4,15 @@ import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+const initialState = {
+  videos: exampleVideoData,
+  video: exampleVideoData[0],
+  // searchValue: ''
+};
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
+
+export default store;
