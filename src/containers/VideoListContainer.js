b800e00a10c0e9b11bde3f1263ece35c84324331
Chris Bell <cb.camellia@gmail.com>
2020-09-02 17:20:14 -0700

    Connect VideoListContainer to redux store

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,9 +2,45 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
-var VideoListContainer = () => {};
+const mapStateToProps = (reduxStore) => {
+  return {
+    videos: reduxStore.videoList
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleVideoListEntryTitleClick: function(video) {
+      dispatch(changeVideo(video));
+    }
+  };
+};
+
+const VideoListContainer = connect(
+  // mapStateToProps = (reduxStore) => {
+  //   return {
+  //     videos: reduxStore.videos
+  //   };
+  // },
+  mapStateToProps,
+  mapDispatchToProps
+)(VideoList);
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
 
 export default VideoListContainer;
+
+
+//the container uses connect to talk to the reduxStore
+
+//it calls two functions when it talke to it
+
+//mapStateToProps  - asks the store for the key name (should match the argument passed into the videoList component ) - want it to return that key name and the stores value
+//example
+// return { votes:  reduxStore.votes}
+// return {videos: reduxStore.videos}
+
+//mapDispatchToProps - that is talking to the store for the second argument which is a function
+//returns an object with the key name that matches the function name  with a value of 'function('whatever arguement the function requires')
+//new object
+//call dispatch('action creator with the argument the function required')
