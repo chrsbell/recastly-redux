6428da7a988e4c8ddaf5c8fb775fd1b2f7a842d4
Chris Bell <cb.camellia@gmail.com>
2020-09-02 17:20:34 -0700

    Begin implementing search action

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,25 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
+  return {
+    type: 'CHANGE_VIDEO_LIST',
+    videos: dispatch(searchYouTube({key: YOUTUBE_API_KEY, query: q}, changeVideoList))
+  };
+
+
+  // {
+  //   video: searchYouTube({key: YOUTUBE_API_KEY, query: q}, changeVideoList),
+  //   type: 'CHANGE_VIDEO_LIST'
+
+  // };
+  // {
+  //   type: 'CHANGE_VIDEO_LIST',
+  // };
   //TODO:  Write an asynchronous action to handle a video search!
 };
 
 export default handleVideoSearch;
+
+
+//'q' is equal to the query of the searchYoutube function
+//YOUTUBE_API_KEY - searchYouTube function key argument
