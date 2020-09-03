041ae14acbceee9ad16b4e3bc1eb7944dc1f9b44
Chris Bell <cb.camellia@gmail.com>
2020-09-02 17:19:10 -0700

    Connect VideoPlayerContainer to redux store

--- src/containers/VideoPlayerContainer.js
+++ src/containers/VideoPlayerContainer.js
@@ -1,7 +1,23 @@
 import { connect } from 'react-redux';
 import VideoPlayer from '../components/VideoPlayer.js';
 
-var VideoPlayerContainer = () => {};
+//() => {};
+
+const mapStateToProps = (reduxStore) => {
+  return {
+    video: reduxStore.currentVideo
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeVideo: function(video) {
+      dispatch(changeVideo(video));
+    }
+  };
+};
+
+var VideoPlayerContainer = connect(mapStateToProps, mapDispatchToProps)(VideoPlayer);
 
 //TODO: define a VideoPlayerContainer component which will hook up your action
 //dispatchers with your VideoPlayer component props.
